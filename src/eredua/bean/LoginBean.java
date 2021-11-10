bc59f947e248fafaf7b18d6cca3352b027bf6c3c
jauregi13 <jon.j1305@gmail.com>
2021-11-10 11:20:03 +0100

    First commit

---
+++ src/eredua/bean/LoginBean.java
@@ -0,0 +1,35 @@
+package eredua.bean;
+
+import java.util.Date;
+
+public class LoginBean {
+	private String izena;
+	private String pasahitza;
+	private Date data;
+	
+	public LoginBean(){
+	}
+	public String getIzena() {
+		return izena;
+	}
+	public void setIzena(String izena) {
+		this.izena = izena;
+	}
+	public String getPasahitza() {
+		return pasahitza;
+	}
+	public void setPasahitza(String pasahitza) {
+		this. pasahitza = pasahitza;
+	}
+	public String egiaztatu() {
+		if (izena.equals("pirata")) return "error";
+		else return "ok";
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+}

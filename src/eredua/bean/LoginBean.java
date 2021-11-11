37618416343df002f1044bc80846905ec3dcf991
jaureg13 <jon.j1305@gmail.com>
2021-11-11 12:17:48 +0100

    JSF laboa 2.zatia bukatzen

--- src/eredua/bean/LoginBean.java
+++ src/eredua/bean/LoginBean.java
@@ -2,11 +2,14 @@ package eredua.bean;
 
 import java.util.Date;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
 public class LoginBean {
 	private String izena;
 	private String pasahitza;
 	private Date data;
-	
+
 	public LoginBean(){
 	}
 	public String getIzena() {
@@ -22,8 +25,15 @@ public class LoginBean {
 		this. pasahitza = pasahitza;
 	}
 	public String egiaztatu() {
-		if (izena.equals("pirata")) return "error";
-		else return "ok";
+		if (izena.length()!=pasahitza.length()){
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage("Errorea: izenaren eta pasahitzaren luzera desberdinak dira."));
+			return null;
+		}
+		if (izena.equals("pirata"))
+			return "error";
+		else
+			return "ok";
 	}
 	public Date getData() {
 		return data;
